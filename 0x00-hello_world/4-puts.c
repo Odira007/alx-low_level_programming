d6f16e216c2803e14864024f9b16b099602b4970
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-09 05:01:00 -0700

    Hello Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
+
 /**
-* main - Entry point
-*
-* Return always 0 (Success)
-*/
+ * main - Put
+ *
+ * Return 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
