34c923ea350084fa1ec970c095b32957a02cd26a
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-09 06:34:47 -0700

    Type sizes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+/**
+ * main - type size
+ *
+ * Description: Print the sizes of data types
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char c;
+	int i;
+	long int li;
+	long long int lli;
+	float f;
+	printf("Size of a char: %lu byte(s)", (unsigned long)sizeof(c));
+	printf("Size of an int: %lu byte(s)", (unsigned long)sizeof(i));
+	printf("Size of a long int: %lu byte(s)", (unsigned long)sizeof(li));
+	printf("Size of a long long int: %lu byte(s)", (unsigned long)sizeof(lli));
+	printf("Size of a float: %lu byte(s)", (unsigned long)sizeof(f));
+	return (0);
+}
