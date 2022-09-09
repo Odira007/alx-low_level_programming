2faf15d4b0ef9a0e9083e40b6f0cdbb29dcd6924
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-09 05:26:03 -0700

    Printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Print program
+ *
+ * Description: print out the string
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("%s", "with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
