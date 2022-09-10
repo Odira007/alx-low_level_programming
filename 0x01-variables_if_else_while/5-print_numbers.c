0da9379c1e9081d38a0ec07a7e7416266f231891
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-10 05:21:33 -0700

    Base 10 Singles

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Base ten
+ *
+ * Description: Print single digit numbers of base ten
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int c;
+
+	c = 0;
+	while (c < 10)
+	{
+		printf("%d", c);
+		c = c + 1;
+	}
+	putchar('\n');
+}
