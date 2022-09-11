0a539ac724089d2f419cbeaeefbcf54fc86d86ba
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-11 12:56:20 -0700

    Two two-digit nums

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -12,10 +12,10 @@ int main(void)
 {
 	int n1 = 0, n2;
 
-	while(n1 <= 99)
+	while (n1 <= 99)
 	{
 		n2 = n1;
-		while(n2 <= 99)
+		while (n2 <= 99)
 		{
 			if (n2 != n1)
 			{
@@ -24,6 +24,7 @@ int main(void)
 				putchar(' ');
 				putchar((n2 / 10) + 48);
 				putchar((n2 % 10) + 48);
+
 				if (n1 != 98 || n2 != 98)
 				{
 					putchar(',');
