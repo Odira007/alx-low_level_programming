b4a08a3fa7f5591fd54f3674a7f4703a6f5953a1
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-13 08:57:07 -0700

    IsLower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <main.h>
+
+/**
+ * _islower - lowercase or not?
+ *
+ * Description: Check if a letter is lowercase or not
+ *
+ * Return: 0 (Success)
+ */
+int _islower(int c)
+{
+	if (c >= a && c <= z)
+	{
+		return (1);
+	} else
+	{
+		return (0);
+	}
+}
