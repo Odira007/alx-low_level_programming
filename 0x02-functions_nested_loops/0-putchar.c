0a2a61b752f503eaa73c7614c26138ba3da40ed9
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-13 05:15:17 -0700

    lowercase alpha

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Printing
+ *
+ * Description: Write a program that prints _putchar
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * print_alphabet - Alpha
+ *
+ * Description: Print the alphabet in lowercase
+ *
+ * Return: 0 (Success)
+ */
+void print_alphabet(void);
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
+void print_alphabet(void)
+{
+	char s;
+
+	s ='a';
+	while(s <= 'z')
+	{
+		putchar(s);
+		s++;
+	}
+	putchar('\n');
+}
