61dfa0dcbddf3a323386bda05115db0bf002bf38
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-13 10:08:38 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_sign - Print sign of a number
+ *
+ * @n: argument for the function
+ *
+ * Return: 0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
