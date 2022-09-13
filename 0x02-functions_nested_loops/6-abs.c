9f7920005be08fd3ad8b8fe7c64ef637bddb5fb2
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-13 10:26:03 -0700

    absolute

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - absolute value
+ *
+ * Description: print the absolute value of a number
+ *
+ * Rerurn: 0
+ */
+int _abs(int n)
+{
+	if (n < 0 || n > 0)
+	{
+		return (n);
+	}
+	else
+		return (0);
+}
