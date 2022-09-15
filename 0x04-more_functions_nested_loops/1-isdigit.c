2e2cd41d742b59b914bea5d48f512282a7a3070a
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-15 04:20:08 -0700

    Multi

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
  * _isdigit - Writes a function that checks for a digit from zero through 9
  * @c: The argument to the function
- * Return: 1 or 0 (success)
+ * Return: 0 (success)
  */
 int _isdigit(int c)
 {
--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -12,4 +12,5 @@ int mul(int a, int b)
 
 	mul = a * b;
 	printf("%d\n", mul);
+	return (mul);
 }
