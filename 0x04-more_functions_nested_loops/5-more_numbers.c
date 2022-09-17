bed477e94af73d7460a8797fb1ff2d0fd65136b4
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-16 18:06:29 -0700

    More nums

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -10,15 +9,15 @@ void more_numbers(void)
 {
 	int n, i;
 
-	for (n = 0; n < 10; n++)
+	for (n = 0; n <= 9; n++)
 	{
-		for (i = 0; i < 14; i++)
+		for (i = 0; i <= 14; i++)
 		{
 			if (i > 9)
 			{
-				putchar((i / 10) + '0');
+				putchar(i / 10 + '0');
 			}
-			putchar((i % 10) + '0');
+			putchar(i % 10 + '0');
 		}
 		putchar('\n');
 	}
