b0a49f81c6bef1db0f786c0427d028b5f976e007
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-20 13:22:46 -0700

    Print that string

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,21 +3,10 @@
 
 /**
  * _puts - write a function that prints a string
- * str - the parameter
+ * @str - the parameter
  *
  * Return: nothing
  */
-void _puts(char *str);
-
-int main(void)
-{
-	char *str;
-
-	str = "My name is Odira";
-	_puts(str);
-	return (0);
-}
-
 void _puts(char *str)
 {
 	int i;
