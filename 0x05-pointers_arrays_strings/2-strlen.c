38b7deb2e78b2e16cffc2f239af2590fd7c0f358
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-19 23:32:47 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -4,10 +4,13 @@
 /**
  * _strlen - function that returns the length of a string
  * @s: parameter
- * Return: always 0
+ * Return: returns len
  */
 int _strlen(char *s)
 {
-	strlen(*s);
-	return (0);
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
 }
