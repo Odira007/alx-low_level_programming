b350df4baa5eb670dc91f930ce087d7af4c689d4
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-22 08:57:05 -0700

    Compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -13,29 +13,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
+	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
 	{
-		if (s1[i] == s2[i] || (s1[i] ^ 32) == s2[i])
-		{
-			continue;
-		}
-		else
-		{
-			break;
-		}
+		i++;
 	}
-		if (s1[i] < s2[i])
-		{
-			return (-15);
-		}
-		else if (s1[i] > s2[i])
-		{
-			return (15);
-		}
-		else if (s1[i] == s2[i])
-		{
-			return (0);
-		}
-		return (0);
+	return (s1[i] - s2[i]);
 }
 
