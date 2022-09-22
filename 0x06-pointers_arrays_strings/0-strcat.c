d6d16aba81447926ad74347128ba45265d418091
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-22 05:03:19 -0700

    Concatenation is cool!

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,17 +7,20 @@
  *
  * @dest: first parameter
  * @src: second parameter
- * Return: returns dest
+ * Return: Always 0
  */
 char *_strcat(char *dest, char *src)
 {
 	int i;
-	int len;
+	int j;
 
-	len = strlen(src);
-	for (i = 0; i < len; i++)
+	for (j = 0; dest[j] != '\0'; j++)
+	{}
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		dest = src[i] + dest;
+
+		dest[j + i] = src[i];
 	}
+	dest[j + i] = '\0';
 	return (dest);
 }
