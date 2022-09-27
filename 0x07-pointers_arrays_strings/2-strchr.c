9fe96dcd9a12bff3613dac99a5204d510ebc3ed2
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-27 13:32:59 -0700

    Even mooooreee

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,12 +1,10 @@
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
 
 /**
  * _strchr -  function that locates a character in a string
  * @s: full string
  * @c: character to be located
- * Return: Returns c
+ * Return: character value
  */
 char *_strchr(char *s, char c)
 {
