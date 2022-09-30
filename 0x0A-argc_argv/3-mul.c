dbd71b8d7a4206ef9e807c7c0005b85dc3760b6e
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-09-30 11:03:59 -0700

    argc and argv are good practice

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -12,18 +12,15 @@ int main(int argc, char *argv[])
 	int i;
 	int mul = 1;
 
-	if (argc < 2)
-	{
-		printf("Error\n");
-		return (1);
-	}
-	else
+	if (argc == 3)
 	{
 		for (i = 1; i < argc; i++)
 		{
 			mul = mul * atoi(argv[i]);
 		}
+		printf("%d\n", mul);
+		return (0);
 	}
-	printf("%d\n", mul);
-	return (0);
+	printf("Error\n");
+	return (1);
 }
