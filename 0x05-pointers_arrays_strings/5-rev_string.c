4825073d144fbfe08a6f2ec584f5755ed37f5ceb
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-10-02 03:24:13 -0700

    Reverse a string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <string.h>
+#include "2-strlen.c"
 
 /**
  * rev_string - Write a function that reverses a string
@@ -9,15 +10,16 @@
 void rev_string(char *s)
 {
 	int i;
-	char t;
+	char rev;
 	int len;
+	int n;
 
 	len = strlen(s);
-	for (i = 0; i < len / 2; i++)
+	n = len / 2;
+	for (i = 0; i < n; i++)
 	{
-		t = s[i];
+		rev  = s[i];
 		s[i] = s[len - i - 1];
-		s[len - i - 1] = t;
+		s[len - i - 1] = rev;
 	}
-	putchar('\n');
 }
