42d317b5230b952b36fe5404cb99d5a5b191683b
Odira Ikewelugo <odiraikewelugo@gmail.com>
2022-10-13 07:20:26 -0700

    Variadic Functions

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -11,8 +11,9 @@ int sum_them_all(const unsigned int n, ...)
 {
 	va_list ap;
 	unsigned int i;
-	unsigned int sum = 0;
-	
+	unsigned int sum;
+
+	sum = 0;
 	if (n == 0)
 		return (0);
 	va_start(ap, n);
